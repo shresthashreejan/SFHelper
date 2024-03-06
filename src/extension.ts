739366d2b5ad17cfb9f4e6d93a514696ea7eb206
shreejan shrestha <shr.shreejan@gmail.com>
2024-03-06 20:47:33 +0545

    Setup sf commands and few optimizations

--- src/extension.ts
+++ src/extension.ts
@@ -1,79 +1,133 @@
 import * as vscode from "vscode";
-
-// ctrl '
+import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
-    let deploy = vscode.commands.registerCommand("sfhelper.deploy", () => {
-        executeCommand("DEPLOY");
+    const commands = [
+        {
+            command: "sfhelper.deploy",
+            action: "deploy",
+            label: "Deploy Active File",
+        },
+        {
+            command: "sfhelper.deployFolder",
+            action: "deployFolder",
+            label: "Deploy Active Folder",
+        },
+        {
+            command: "sfhelper.retrieve",
+            action: "retrieve",
+            label: "Retrieve Active File",
+        },
+        {
+            command: "sfhelper.retrieveFolder",
+            action: "retrieveFolder",
+            label: "Retrieve Active Folder",
+        },
+        {
+            command: "sfhelper.runTest",
+            action: "runTest",
+            label: "Run Active Test Class",
+        },
+        {
+            command: "sfhelper.deleteDebugLogs",
+            action: "deleteDebugLogs",
+            label: "Delete All Debug Logs",
+        },
+    ];
+
+    commands.forEach(({ command, action }) => {
+        context.subscriptions.push(
+            vscode.commands.registerCommand(command, () => {
+                if (action === "deleteDebugLogs") {
+                    deleteLogs();
+                } else {
+                    executeCommand(action);
+                }
+            })
+        );
     });
 
-    let retrieve = vscode.commands.registerCommand("sfhelper.retrieve", () => {
-        executeCommand("RETRIEVE");
-    });
-
-    let runTest = vscode.commands.registerCommand("sfhelper.runTest", () => {
-        executeCommand("RUN TEST");
-    });
-
-    let deleteDebugLogs = vscode.commands.registerCommand(
-        "sfhelper.deleteDebugLogs",
-        () => {
-            executeCommand("DELETE DEBUG LOGS");
-        }
-    );
+    let items = commands.map(({ label, action }) => ({
+        label: `SF Helper: ${label}`,
+        action: `${action}`,
+    }));
 
     let openDropdown = vscode.commands.registerCommand(
         "sfhelper.openDropdown",
         () => {
-            const items = [
-                {
-                    label: "SF Helper: Deploy Active File",
-                    command: "sfhelper.deploy",
-                },
-                {
-                    label: "SF Helper: Retrieve Active File",
-                    command: "sfhelper.retrieve",
-                },
-                {
-                    label: "SF Helper: Run Active Test Class",
-                    command: "sfhelper.runTest",
-                },
-                {
-                    label: "SF Helper: Delete All Debug Logs",
-                    command: "sfhelper.deleteDebugLogs",
-                },
-            ];
-
             vscode.window.showQuickPick(items).then((selectedItem) => {
-                if (selectedItem) {
-                    vscode.commands.executeCommand(selectedItem.command);
+                if (selectedItem && selectedItem.action === "deleteDebugLogs") {
+                    deleteLogs();
+                } else if (selectedItem) {
+                    executeCommand(selectedItem.action);
                 }
             });
         }
     );
-
-    context.subscriptions.push(deploy);
-    context.subscriptions.push(retrieve);
-    context.subscriptions.push(runTest);
-    context.subscriptions.push(deleteDebugLogs);
     context.subscriptions.push(openDropdown);
 }
 
 function executeCommand(command: string) {
     const editor = vscode.window.activeTextEditor;
+    let cmdPrefix: string = "";
+    let cmdSuffix: string = "";
+
     if (!editor) {
         vscode.window.showErrorMessage("No active editor found.");
         return;
     }
+    let filePath = editor.document.uri.fsPath;
 
-    const filePath = editor.document.uri.fsPath;
+    switch (command) {
+        case "deploy":
+            cmdPrefix = "sf project deploy start -d";
+            cmdSuffix = "-c";
+            break;
+        case "deployFolder":
+            cmdPrefix = "sf project deploy start -d";
+            cmdSuffix = "-c";
+            filePath = path.dirname(filePath);
+            break;
+        case "retrieve":
+            cmdPrefix = "sf project retrieve start -d";
+            cmdSuffix = "-c";
+            break;
+        case "retrieveFolder":
+            cmdPrefix = "sf project retrieve start -d";
+            cmdSuffix = "-c";
+            filePath = path.dirname(filePath);
+            break;
+        case "runTest":
+            cmdPrefix = "sf apex run test -n";
+            cmdSuffix = "-r human -c -y";
+            break;
+        default:
+            break;
+    }
 
     let terminal = vscode.window.activeTerminal;
     if (!terminal) {
         terminal = vscode.window.createTerminal("SFHelper");
     }
 
-    terminal.sendText(`echo "${command}: ${filePath}"`);
+    terminal.sendText(`${cmdPrefix} ${filePath} ${cmdSuffix}`);
+    terminal.show();
+}
+
+function deleteLogs() {
+    let terminal = vscode.window.activeTerminal;
+    if (!terminal) {
+        terminal = vscode.window.createTerminal("SFHelper");
+    }
+    terminal.sendText(
+        `sf data query -q "SELECT Id FROM ApexLog ORDER BY loglength DESC" -r "csv" | out-file -encoding oem debugLogs.csv | sf data delete bulk -s ApexLog -f debugLogs.csv`
+    );
+
+    setTimeout(() => {
+        if (terminal) {
+            terminal.sendText(`del debugLogs.csv`);
+        }
+    }, 5000);
     terminal.show();
 }
 
